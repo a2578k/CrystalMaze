3588131a46b2b8a4c16b9c0f22e0824614eb0c92
s.sogawa <a2578k@gmail.com>
2016-02-20 12:52:36 +0900

    init

---
+++ Assets/MouseGui.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MouseGui : MonoBehaviour,IPointerDownHandler,IDragHandler  {
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		//throw new System.NotImplementedException ();
+		Debug.Log("OnDrag");
+	}
+
+	#endregion
+
+	#region IPointerDownHandler implementation
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		//throw new System.NotImplementedException ();
+		Debug.Log("OnPointerDown");
+	}
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
